ba0ef0d0015d0a28c0ff60287643418c5199f7cf
tuxorials2 <84857461+tuxorials2@users.noreply.github.com>
2022-03-03 18:49:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
-mkdir .tmp-install && cd .tmp-install
-curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
-chmod +x openvpn-install.sh
-export AUTO_INSTALL=y
-export APPROVE_INSTALL=y
-export APPROVE_IP=y
-export IPV6_SUPPORT=n
-export PORT_CHOICE=1
-export PROTOCOL_CHOICE=1
-export DNS=1
-export COMPRESSION_ENABLED=n
-export CUSTOMIZE_ENC=n
-export CLIENT=vpn
-export PASS=1
-./openvpn-install.sh > /dev/null
-print "BEGIN OPENVPN-FILE"
-cat vpn.ovpn
-touch ~/.vpn_installed
-cd
+if (-f ~/.vpn_installed)
+then
+	echo "BEGIN OPENVPN-FILE"
+	cat .vpn.ovpn
+else
+	mkdir .tmp-install && cd .tmp-install
+	curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
+	chmod +x openvpn-install.sh
+	export AUTO_INSTALL=y
+	export APPROVE_INSTALL=y
+	export APPROVE_IP=y
+	export IPV6_SUPPORT=n
+	export PORT_CHOICE=1
+	export PROTOCOL_CHOICE=1
+	export DNS=1
+	export COMPRESSION_ENABLED=n
+	export CUSTOMIZE_ENC=n
+	export CLIENT=vpn
+	export PASS=1
+	./openvpn-install.sh > /dev/null
+	echo "BEGIN OPENVPN-FILE"
+	cat vpn.ovpn
+	mv vpn.ovpn ~/.vpn.ovpn
+	touch ~/.vpn_installed
+	cd
+	rm .tmp-install
+fi
